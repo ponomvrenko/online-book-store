c8c52b0b0eef1a999e481c5dc392b9d970d05388
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-21 15:34:36 +0200

    Added Role entity.

---
+++ src/main/java/com/example/onlinebookstore/model/Role.java
@@ -0,0 +1,25 @@
+package com.example.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private enum RoleName {
+        ADMIN,
+        USER
+    }
+}
