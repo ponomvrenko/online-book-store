0bfc5b905896cd89bc3f63c41f438843745ed37c
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-26 21:54:14 +0200

    Added @EnableMethodSecurity to SecurityConfig

--- src/main/java/com/example/onlinebookstore/security/SecurityConfig.java
+++ src/main/java/com/example/onlinebookstore/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     @Value("${public.endpoints}")
     private String[] publicEndpoints;
