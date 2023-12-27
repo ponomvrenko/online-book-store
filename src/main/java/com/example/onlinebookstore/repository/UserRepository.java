c29efca6b10ed5e6178cf5c988ad79ff8b6f997d
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-27 16:42:57 +0200

    Fixes: - Avoid hardcoding in 8th liquibase script by using sql queries. - Added @Repository in RoleRepository. - Removed /api/ from public endpoints cause we already have it as servlet context path. - Added EntityGraph that helps to avoid LazyInitializationException.

--- src/main/java/com/example/onlinebookstore/repository/UserRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 }
