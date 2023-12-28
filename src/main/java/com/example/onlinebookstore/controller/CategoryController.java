8fa6fc79b616b7fa2db4ec5bee704fa59cb68bd6
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-28 15:08:54 +0200

    Created CategoryController with all necessary endpoints.

---
+++ src/main/java/com/example/onlinebookstore/controller/CategoryController.java
@@ -0,0 +1,86 @@
+package com.example.onlinebookstore.controller;
+
+import com.example.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
+import com.example.onlinebookstore.dto.category.CategoryRequestDto;
+import com.example.onlinebookstore.dto.category.CategoryResponseDto;
+import com.example.onlinebookstore.service.BookService;
+import com.example.onlinebookstore.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Category management", description = "Endpoints for managing categories")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final BookService bookService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
+    @Operation(summary = "Get all categories",
+            description = "Get a list of all existing categories")
+    public List<CategoryResponseDto> getAll(Pageable pageable) {
+        return categoryService.getAll(pageable);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create new category",
+            description = "Create a new category")
+    public CategoryResponseDto createCategory(@RequestBody @Valid CategoryRequestDto requestDto) {
+        return categoryService.save(requestDto);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
+    @Operation(summary = "Get category by ID",
+            description = "Return a category by identifier")
+    public CategoryResponseDto getCategoryById(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = "Update category by ID",
+            description = "Updates category parameters with a new one")
+    public CategoryResponseDto updateCategory(@PathVariable Long id,
+                                              @RequestBody @Valid CategoryRequestDto requestDto) {
+        return categoryService.updateById(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete category by ID",
+            description = "Removes the category")
+    public void deleteCategoryById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
+
+    @GetMapping("/{id}/books")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
+    @Operation(summary = "Get books by category ID",
+            description = "Return a list of all books that matches this category ID")
+    public List<BookDtoWithoutCategoryIds> getBooksByCategoryId(
+            @PathVariable(name = "id") Long categoryId
+    ) {
+        return bookService.findAllByCategoryId(categoryId);
+    }
+}
