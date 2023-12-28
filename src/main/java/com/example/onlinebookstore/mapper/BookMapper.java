7f0ecb575a234458b044464b1dfa301add68b237
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-28 14:51:49 +0200

    Modified the BookMapper class & added CategoryMapper.

--- src/main/java/com/example/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/example/onlinebookstore/mapper/BookMapper.java
@@ -1,14 +1,33 @@
 package com.example.onlinebookstore.mapper;
 
 import com.example.onlinebookstore.config.MapperConfig;
-import com.example.onlinebookstore.dto.book.BookDto;
+import com.example.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
+import com.example.onlinebookstore.dto.book.BookResponseDto;
 import com.example.onlinebookstore.dto.book.CreateBookRequestDto;
 import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.model.Category;
+import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
-    BookDto toDto(Book book);
+    @Mapping(target = "categoryIds", ignore = true)
+    BookResponseDto toDto(Book book);
 
-    Book toModel(CreateBookRequestDto bookRequestDto);
+    @Mapping(target = "categories", ignore = true)
+    Book toEntity(CreateBookRequestDto bookRequestDto);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookResponseDto bookResponseDto, Book book) {
+        if (!book.getCategories().isEmpty()) {
+            bookResponseDto.setCategoryIds(book.getCategories().stream()
+                    .map(Category::getId)
+                    .collect(Collectors.toSet()));
+        }
+    }
 }
--- src/main/java/com/example/onlinebookstore/mapper/CategoryMapper.java
+++ src/main/java/com/example/onlinebookstore/mapper/CategoryMapper.java
@@ -1,14 +1,14 @@
 package com.example.onlinebookstore.mapper;
 
 import com.example.onlinebookstore.config.MapperConfig;
-import com.example.onlinebookstore.dto.category.CategoryDto;
 import com.example.onlinebookstore.dto.category.CategoryRequestDto;
+import com.example.onlinebookstore.dto.category.CategoryResponseDto;
 import com.example.onlinebookstore.model.Category;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface CategoryMapper {
-    CategoryDto toDto(Category category);
+    CategoryResponseDto toDto(Category category);
 
     Category toEntity(CategoryRequestDto requestDto);
 }
