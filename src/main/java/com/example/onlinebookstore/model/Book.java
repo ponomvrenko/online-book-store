0895df4f63e446e34537ebf3c42a699529966d8e
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-28 14:58:42 +0200

    Optimized imports in Book model.

--- src/main/java/com/example/onlinebookstore/model/Book.java
+++ src/main/java/com/example/onlinebookstore/model/Book.java
@@ -1,11 +1,17 @@
 package com.example.onlinebookstore.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
