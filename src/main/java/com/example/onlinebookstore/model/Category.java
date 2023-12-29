3f12a17b123ff4c9fc6a887488728c00833c311d
Danil Ponomarenko <akame.dante13@gmail.com>
2023-12-29 08:55:35 +0200

    Improving Category entity after mentor review. Removed nullable constraint in primary key at liquibase script.

--- src/main/java/com/example/onlinebookstore/model/Category.java
+++ src/main/java/com/example/onlinebookstore/model/Category.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -14,6 +15,7 @@ import org.hibernate.annotations.Where;
 @Data
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
+@NoArgsConstructor
 @Table(name = "categories")
 public class Category {
     @Id
@@ -21,6 +23,7 @@ public class Category {
     private Long id;
     @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String description;
     @Column(nullable = false)
     private boolean isDeleted;
