30bf22ad5a5e6297b5e84cdacdcc0e0df060dcbe
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-04 01:58:08 +0200

    CartItem & ShoppingCart: Improved table naming, added JoinColumn's for entity relations + naming inside DB. Also added soft delete & isDeleted fields.

--- src/main/java/com/example/onlinebookstore/model/CartItem.java
+++ src/main/java/com/example/onlinebookstore/model/CartItem.java
@@ -1,18 +1,24 @@
 package com.example.onlinebookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
-@Table(name = "cart_item")
+@SQLDelete(sql = "UPDATE cart_items SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "cart_items")
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,10 +26,14 @@ public class CartItem {
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @ManyToOne
+    @JoinColumn(name = "shopping_cart_id")
     private ShoppingCart shoppingCart;
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @ManyToOne
+    @JoinColumn(name = "book_id")
     private Book book;
     private int quantity;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
