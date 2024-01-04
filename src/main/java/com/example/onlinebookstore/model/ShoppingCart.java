43fcae7faa234e03d7ae71f7009b9ec6830bba11
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-04 07:08:01 +0200

    Added new HashSet<> to cartItems Set.

--- src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -33,7 +34,7 @@ public class ShoppingCart {
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "shoppingCart")
-    private Set<CartItem> cartItems;
+    private Set<CartItem> cartItems = new HashSet<>();
     @Column(nullable = false)
     private boolean isDeleted = false;
 }
