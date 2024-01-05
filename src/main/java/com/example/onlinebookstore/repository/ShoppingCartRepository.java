a691ab4cae30a0e1c961c88f050c7a5a5f612055
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-05 16:33:14 +0200

    Cause of fetchType.Lazy in `@ManyToOne` inside `CartItem` we created SQL query that matches with `findByUserIdWithCartItems` method in `ShoppingCartRepository`.

--- src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
@@ -2,12 +2,13 @@ package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.ShoppingCart;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @EntityGraph(attributePaths = {"cartItems", "cartItems.book"})
+    @Query("SELECT sc FROM ShoppingCart sc JOIN FETCH sc.cartItems ci "
+            + "LEFT JOIN ci.book WHERE sc.user.id = :userId")
     Optional<ShoppingCart> findByUserIdWithCartItems(Long userId);
 }
