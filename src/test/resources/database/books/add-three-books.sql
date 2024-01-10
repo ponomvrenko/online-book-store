fb4c650b7a9f4bed94875e56eb17e2492c65fee8
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-10 07:37:54 +0200

    Added sql scripts for tests. Commented docker dependency.

---
+++ src/test/resources/database/books/add-three-books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (id, title, author, isbn, price, description) VALUES (1, 'Critical thinking', 'Jonathan Haber', '978-617-8025-53-3', 400, 'Be critical to everything')
+INSERT INTO books (id, title, author, isbn, price, description) VALUES (2, 'Visual culture', 'Alexis L. Boylen', '978-617-7799-65-7', 390, 'Visual culture is not neutral')
+INSERT INTO books (id, title, author, isbn, price, description) VALUES (3, 'Post-truth', 'Lee McIntyre', '978-617-7799-76-3', 450, 'What is post-truth?')
